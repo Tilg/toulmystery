4e1bdba5b2d00976c98618e973e87c7b0e7ddec1
florent gerard <gerar1501@gmail.com>
2013-11-06 14:33:11 +0100

    dernier commit avant changement de branche

---
+++ Assets/Models/GameModule.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class GameModule {
+
+	string title;
+	string description;
+	string soundUrl;
+	
+	public GameModule(string _title, string _description, string _soundUrl){
+		
+	}	
+	
+	
+	
+}

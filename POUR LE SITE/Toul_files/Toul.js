073de66f93699f272559f0cc39b56e89e7043dac
Hadrien Torres <hadrien.torres@gmail.com>
2014-02-12 10:55:55 +0100

    Suppression éléments dossier unity inutiles
    Suppression du dossier "POUR LE SITE"

--- POUR LE SITE/Toul_files/Toul.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20101015
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Toul_files/ToulMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
